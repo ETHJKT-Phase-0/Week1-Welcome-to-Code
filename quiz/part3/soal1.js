663842b1ec3a2509c86321d16c1e4221b912c2c9
Nazwa-Wijaya <nazwawijaya89@gmail.com>
2025-02-28 14:42:13 +0700

    Push part3 week1

---
+++ quiz/part3/soal1.js
@@ -0,0 +1,23 @@
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while. 
+// Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
+// menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+//tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+//looping LOOPING PERTAMA
+
+console.log("Looping pertama")
+let a = 0
+while(a < 10){
+    console.log(a + '  Random Para Nolep')
+    a++
+}
+
+console.log(" ")
+
+console.log('Looping Kedua')
+let b = 10
+while(b > 0){
+    console.log(b + '  Masih Phase 0 sir')
+    b--
+}
