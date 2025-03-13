3ca63cc291c745c64de06e8d32f4e8252a44c79e
vigorjs <111201912289@mhs.dinus.ac.id>
2025-03-14 01:06:42 +0800

    feat: done week1

--- quiz/part3/part3.js
+++ quiz/part3/part3.js
@@ -1,33 +1,33 @@
-// //soal 1
-// console.log("LOOPING PERTAMA");
-// i = 0;
-// while (i <= 10) {
-//   console.log(i++);
-// }
+//soal 1
+console.log("LOOPING PERTAMA");
+i = 0;
+while (i <= 10) {
+  console.log(i++);
+}
 
-// console.log("LOOPING KEDUA");
-// i = 10;
-// while (i >= 0) {
-//   console.log(i--);
-// }
+console.log("LOOPING KEDUA");
+i = 10;
+while (i >= 0) {
+  console.log(i--);
+}
 
-// //soal 2
-// console.log("LOOPING PERTAMA");
-// for (i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
+//soal 2
+console.log("LOOPING PERTAMA");
+for (i = 0; i <= 10; i++) {
+  console.log(i);
+}
 
-// console.log("LOOPING KEDUA");
-// for (i = 10; i >= 0; i++) {
-//   console.log(i);
-// }
+console.log("LOOPING KEDUA");
+for (i = 10; i >= 0; i++) {
+  console.log(i);
+}
 
-// //soal 3
-// counter = 1;
-// while (counter <= 100) {
-//   console.log(counter % 2 == 0 ? "GENAP" : "GANJIL");
-//   counter++;
-// }
+//soal 3
+counter = 1;
+while (counter <= 100) {
+  console.log(counter % 2 == 0 ? "GENAP" : "GANJIL");
+  counter++;
+}
 
 //soal4
 counter2 = 0;
