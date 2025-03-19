9d1b834d49b2814cc52e7f77daf8c0a6938095cf
jidan76 <142732910+jidan76@users.noreply.github.com>
2025-03-19 21:31:49 +0700

    Delete Week1-Welcome-to-Code directory

--- Week1-Welcome-to-Code/quiz/part4/soal1.js
+++
@@ -1,9 +0,0 @@
-// Tugas 1
-// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", 
-// yang kemudian akan ditampilkan di console.
-
-function shoutOut(){
-    return "Halo function!";
-}
-
-console.log(shoutOut());
