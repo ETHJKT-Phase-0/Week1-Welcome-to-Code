987c694c9787353c3d4d126a66bb9a55cf673483
Aahome <azizhanxk@gmail.com>
2025-04-14 21:53:16 +0700

    push ujian ke-4(memang saya lupa kerjakan wkwk)

--- quiz/ujian/soal4.js
+++ quiz/ujian/soal4.js
@@ -5,8 +5,20 @@
 //   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
 
 function xo(str) {
-    // you can only write your code here!
+    let xCount = 0;
+    let oCount = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'x' || str[i] === 'X') { 
+            xCount++;
+        } else if (str[i] === 'o' || str[i] === 'O') {
+            oCount++;
+        }
+    }
+  
+    return xCount === oCount;
 }
+  
   
 // TEST CASES
 console.log(xo('xoxoxo')); // true
@@ -14,3 +26,6 @@ console.log(xo('oxooxo')); // false
 console.log(xo('oxo')); // false
 console.log(xo('xxxooo')); // true
 console.log(xo('xoxooxxo')); // true
+// +
+console.log(xo('xoxxo')); // false
+console.log(xo('xoxooxooxxxo')); // true
