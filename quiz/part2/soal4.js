668f9fc58418bbf2492299830a54af7aa5d72134
alfakautsar7 <143330282+alfakautsar7@users.noreply.github.com>
2025-04-29 14:31:05 +0700

    update quiz/part2/soal4.js

--- quiz/part2/soal4.js
+++ quiz/part2/soal4.js
@@ -1,8 +1,7 @@
 // Problem
 // Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
 
-
-let word4 = 'wow JavaScript is so cool';
+let word4 = "wow JavaScript is so cool";
 let exampleFirstWord4 = word4.substring(0, 3);
 let exampleSecondWord4 = word4.substring(4, 14);
 let exampleThirdWord4 = word4.substring(15, 17);
@@ -15,8 +14,18 @@ let thirdWordLength = exampleThirdWord4.length;
 let fourthWordLength = exampleFourthWord4.length;
 let fivethWordLength = exampleFivethWord4.length;
 
-console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
-console.log('Second Word: ' + exampleSecondWord4 + ', with length: ' + secondWordLength);
-console.log('Third Word: ' + exampleThirdWord4 + ', with length: ' + thirdWordLength);
-console.log('Fourth Word: ' + exampleFourthWord4 + ', with length: ' + fourthWordLength);
-console.log('Fiveth Word: ' + exampleFivethWord4 + ', with length: ' + fivethWordLength);
+console.log(
+    "First Word: " + exampleFirstWord4 + ", with length: " + firstWordLength
+);
+console.log(
+    "Second Word: " + exampleSecondWord4 + ", with length: " + secondWordLength
+);
+console.log(
+    "Third Word: " + exampleThirdWord4 + ", with length: " + thirdWordLength
+);
+console.log(
+    "Fourth Word: " + exampleFourthWord4 + ", with length: " + fourthWordLength
+);
+console.log(
+    "Fiveth Word: " + exampleFivethWord4 + ", with length: " + fivethWordLength
+);
