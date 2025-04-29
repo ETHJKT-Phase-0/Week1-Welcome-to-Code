942bafce0ee9a10f614a9c3233952f5a26b24dd0
alfakautsar7 <143330282+alfakautsar7@users.noreply.github.com>
2025-04-29 14:06:12 +0700

    update quiz/part2/soal2.js

--- quiz/part2/soal2.js
+++ quiz/part2/soal2.js
@@ -7,13 +7,39 @@
 // Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
 //  tersebut!
 
-let word = 'wow JavaScript is so cool';
-let exampleFirstWord = word[0] + word[1] + word[2];
-let exampleSecondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13];
-let exampleThirdWord = word[0] + word[1] + word[2];
-let exampleFourthWord = word[0] + word[1] + word[2];
-let exampleFivethWord = word[0] + word[1] + word[2];
+let word = "wow JavaScript is so cool";
+let exampleFirstWord = 
+    word[0] + 
+    word[1] + 
+    word[2];
+let exampleSecondWord =
+    word[4] +
+    word[5] +
+    word[6] +
+    word[7] +
+    word[8] +
+    word[9] +
+    word[10] +
+    word[11] +
+    word[12] +
+    word[13];
+let exampleThirdWord = 
+    word[15] + 
+    word[16];
+let exampleFourthWord = 
+    word[18] + 
+    word[19];
+let exampleFivethWord = 
+    word[21] + 
+    word[22] + 
+    word[23] + 
+    word[24];
 
-console.log('First Word: ' + exampleFirstWord);
-console.log('Second Word: ' + exampleSecondWord);
+console.log("First Word: " + 
+  exampleFirstWord);
+console.log("Second Word: " + exampleSecondWord);
+console.log("Third Word: " +
+  exampleThirdWord);
+console.log("Fourth Word: " + exampleFourthWord);
+console.log("Fiveth Word: " + exampleFivethWord);
 //lanjutkan dengan struktur log diatas
