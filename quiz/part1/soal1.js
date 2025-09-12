f56d700e93237692ce787a30ff1105e2239bd05f
Muhammad Dimas Saputra <mdimas.saputra1006@gmail.com>
2025-09-12 18:55:10 +0700

    Submit tugas pertama

--- quiz/part1/soal1.js
+++ quiz/part1/soal1.js
@@ -18,13 +18,23 @@
 // algoritma
 //isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
 
-/* Jadi algoritma yang saya pakai adalah if else if 
-if else if ini akan mengecek kondisi satu persatu secara berurutan
-dimulai dari kondisi pertama, jika kondisi pertama terpenuhi
-maka blok kode di dalamnya akan dijalankan, dan pengecekan kondisi
-berhenti di situ. Namun, jika kondisi pertama tidak terpenuhi,
-maka program akan melanjutkan ke kondisi berikutnya, dan seterusnya
-sama seperti program klasifikasi nilai ulangan yang pernah saya buat
+/* Jadi algoritma yang saya pakai adalah logika percabangan
+jadi pertama saya akan mengecek apakah variabel nama itu kosong atau tidak
+jika kosong maka akan menampilkan "nama wajib diisi"
+jika tidak kosong maka akan lanjut ke pengecekan variabel peran
+sama seperti variabel nama, jika variabel peran kosong maka akan menampilkan
+"Pilih Peranmu untuk memulai game"
+jika tidak kosong maka akan lanjut ke pengecekan value peran
+nah tapi untuk yang ini saya menggunakan if else if else
+karena ini tuh mirip dengan project yang saya pernah saya kerjakan yaitu pengklasifikasian nilai
+jika peran adalah Ksatria maka akan menampilkan
+"halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!" 
+jika peran adalah Tabib maka akan menampilkan
+"halo Tabib ${nama} , kamu akan membantu temanmu yang terluka"
+jika peran adalah Penyihir maka akan menampilkan
+"halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!"
+jika peran bukan salah satu dari ketiga peran diatas maka akan menampilkan
+"tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada"
 dan disini ada 6 kondisi yang harus di cek
 1. jika nama kosong, maka tampilkan "nama wajib diisi"
 2. jika peran kosong, maka tampilkan "Pilih Peranmu untuk memulai game"
@@ -39,7 +49,9 @@ let nama = "DIMAS", peran = "Tabib";
 //code disini gunakan console.log untuk outputnya
 if (nama === "") {
   console.log("nama wajib diisi");
-} else if (peran === "") {
+} 
+
+if (peran === "") {
   console.log("Pilih Peranmu untuk memulai game");
 } 
 
