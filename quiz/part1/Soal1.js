b5923a4d3dd9f573728524dada785595b93fed72
irhamchandra99-crypto <irhamchandra99@gmail.com>
2025-12-18 22:02:03 +0700

    ada revisi dibagian algoritma

--- quiz/part1/Soal1.js
+++ quiz/part1/Soal1.js
@@ -19,6 +19,18 @@
 
 // algoritma
 //isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
+// Algoritma Program sebagai berikut : 
+/*
+1. buat 2 variabel berupa nama dan peran
+2. lalu variabel tersebut akan dideteksi
+3. jika variabel {nama} kosong maka akan muncul tulisan nama wajib diisi, yang artinya ya namanya wajib ada
+4. jika sudah diisi lanjut ke program berikutnya yaitu peran, untuk peran ada 3 yaitu ksatria, tabib, penyihir. 
+5. jika {peran} tidak memiliki value maka program akan menyuruh wajib mengisi peran
+6. lalu ketika {peran} diisi dengan ksatria maka akan muncul output sebagai : halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!
+7. jika {peran} diisi dengan tabib maka akan muncul output sebagai : halo Tabib ${nama} , kamu akan membantu temanmu yang terluka
+8. jika {peran} diisi dengan penyihir maka akan muncul output sebagai : halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!
+9. tapi jika {peran} diisi dengan diluar 3 opsi tersebut maka akan muncul output sebagai berikut : tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada
+*/
 
 
 let nama = "", peran = "";
