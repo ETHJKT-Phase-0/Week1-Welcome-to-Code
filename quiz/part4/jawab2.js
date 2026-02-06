c1224e2c856dadd1273b773bdf36a8b6731ca3e4
Indayani <davanalendra2@gmail.com>
2026-02-06 14:47:45 +0700

    finishing question2 part4

--- quiz/part4/jawab2.js
+++ quiz/part4/jawab2.js
@@ -2,9 +2,12 @@
 // dua parameter yang dikirim.
 
 //bikin fungsinya disini
-
 let num1 = 1
 let num2 = 2
+
+function calculateMultiply(num1, num2) {
+    return num1 * num2;
+}
 
 let hasilPerkalian = calculateMultiply(num1,num2);
 console.log(hasilPerkalian);
